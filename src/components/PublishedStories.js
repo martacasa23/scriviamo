5c5e64f9722fd327e7d68bc46706e46c3c52bc73
Marta <martacasadei97@gmail.com>
2024-12-05 11:49:25 +0100

    Publishedstory

--- src/components/PublishedStories.js
+++ src/components/PublishedStories.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';  // Importa Link per la navigazione tra le pagine
 
-
 function PublishedStories() {
   const [stories, setStories] = useState([]);
 
@@ -23,33 +22,32 @@ function PublishedStories() {
     };
 
     fetchStories();
-  }, []);
+  }, [backendUrl]); // Aggiungi backendUrl qui per risolvere l'errore
 
   return (
-    <div className="story-container bg-warning-subtle  ">
-    <h1 className="text-center ">Storie Pubblicate</h1>
-    <div className="row justify-content-center"> {/* Rendi le card parte di una griglia */}
-      {stories.length > 0 ? (
-        stories.map((story) => (
-          <div className="col-md-6 col-lg-4 mb-4 d-flex justify-content-center" key={story.id}> {/* Card centrata */}
-            <div className="card border-dark text-center" style={{ width: '18rem' }}>
-              <div className="card-body">
-                <h5 className="card-title">{story.title}</h5>
-                <p className="card-text">Autore: {story.name}</p>
-                <p className="card-text">{story.content.substring(0, 100)}...</p>
-                <Link to={`/story/${story.id}`} className="btn btn-outline-dark">
-                  Leggi la storia
-                </Link>
+    <div className="story-container bg-warning-subtle">
+      <h1 className="text-center">Storie Pubblicate</h1>
+      <div className="row justify-content-center">
+        {stories.length > 0 ? (
+          stories.map((story) => (
+            <div className="col-md-6 col-lg-4 mb-4 d-flex justify-content-center" key={story.id}>
+              <div className="card border-dark text-center" style={{ width: '18rem' }}>
+                <div className="card-body">
+                  <h5 className="card-title">{story.title}</h5>
+                  <p className="card-text">Autore: {story.name}</p>
+                  <p className="card-text">{story.content.substring(0, 100)}...</p>
+                  <Link to={`/story/${story.id}`} className="btn btn-outline-dark">
+                    Leggi la storia
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))
-      ) : (
-        <p>Non ci sono storie pubblicate al momento.</p>
-      )}
+          ))
+        ) : (
+          <p>Non ci sono storie pubblicate al momento.</p>
+        )}
+      </div>
     </div>
-  </div>
-  
   );
 }
 
